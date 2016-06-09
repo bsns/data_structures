5372aa845f6f1e7841da7195a07e2e19f963953d
bsns <bsns@live.com>
2016-06-09 14:26:35 +0800

    selection

---
+++ sort/selection.cc
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+void selection(int Array[])
+{
+    int tempvalue;
+    int length=sizeof(Array);
+    for(int ii=0;ii<=length;ii++)
+    for(int jj=ii;jj<=length;jj++)
+    if(Array[jj]<Array[ii])
+    {
+        tempvalue=Array[ii];
+        Array[ii]=Array[jj];
+        Array[jj]=tempvalue;
+    }
+}
+
+int main()
+{
+    int A[]={49,37,68,72,13,45,03};
+    selection(A);
+    for(int kk=0;kk<=6;kk++)
+    cout<<" num"<<kk<<"="<<A[kk];
+    return 0;
+}
