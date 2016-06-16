380b23a0e7a4672355f4851949ca275cfce54f5d
bsns <bsns@live.com>
2016-06-16 17:46:09 +0800

    change readme

--- LinearList/singlelist.cc
+++ LinearList/singlelist.cc
@@ -11,9 +11,10 @@ friend class singlelist;
 class singlelist{
 public:
      singlelist(){createlist();}
-     //~singlelist(){};
+     ~singlelist();
     void createlist();
     bool find(int n,int x);
+    bool isEmpty();
     int search(int x);
     bool append(int x);
     bool insert(int n,int x);
@@ -23,15 +24,26 @@ public:
     void output();
     int length=0;
     private:
-    node *head;
     node *first;
 };
 void singlelist::createlist(){
-    first->elements=0;
+    //first->elements=0;
+    node *first=new node;
     first->next=NULL;
+    //first->next=NULL;
 }
+bool singlelist::isEmpty(){
+    return length==0;
+}
+singlelist::~singlelist(){
+    node *current;
+    while(first){
+        current=first->next;
+        delete first;
+        first=current;
+    }}
 bool singlelist::find(int n,int x){
-    if(n<0||n>n-1)
+    if(n<0||n>length-1)
     {cout<<"OUT BOUND ";return 0;}
     node *p=first;
     for(int ll=0;ll,n;ll++)
@@ -45,31 +57,38 @@ int singlelist::search(int x){
     }
 
 bool singlelist::append(int x){
-    node *newnode=new node;
-    newnode->elements=x;
-    newnode->next=NULL;
-    node *p=first;
-    if(length==0)
-    {first->next=newnode;}
-    else
-    while(p->next!=NULL)        //until the end of the list
-    {
-    p=p->next;}
-    p->next=newnode;            //add the new node ate the end
-    length++;
+   // node *newnode=new node;
+   // newnode->elements=x;
+   // newnode->next=NULL;
+   // node *p=first;
+    //if(length==0)
+    //{first->next=newnode;}
+    //vmlinuz.oldelse
+   // while(p->next!=NULL)        //until the end of the list
+   //if(length==0){first->next=newnode;}
+   //else
+   //for(int kk=1;kk<length;kk++){
+    //p=p->next;
+    //p->next=newnode;}            //add the new node ate the end
+    //length++;
+    insert(length,x);
 }
 int singlelist::getlength(){
     cout<<"length="<<length<<endl;
     return length;
 }
 bool singlelist::insert(int n,int x){
-    if(n<0||n>length-1) {cout<<"OUT BOUND";return 0;}
+    if(n<0) {cout<<"OUT BOUND";return 0;}
     node *s=first;node *t=new node;
     t->elements=x;
-    for(int nn=0;nn<n;nn++){
+    //if(n==0){
+    //t=first->next;//return 1;
+    //}
+    for(int jj=0;jj<=n;jj++){
     s=s->next;
     }
-    t->next=s->next;            //first link the node (after the posiotion where insert) with the new node
+    t->next=s->next;
+    //first link the node (after the posiotion where insert) with the new node
     s->next=t;                  //link the front node to the new node
     length++;
     return 1;
@@ -111,16 +130,17 @@ int main()
 {
     cout<<"go";
     singlelist B;
-    for(int ii=0;ii<10;ii++)
-    {
-        B.append(ii);
-    }
+    //for(int ii=0;ii<10;ii++)
+    //{
+    //    B.insert(ii,ii);
+    //}
+    B.insert(1,7);
         B.output();
         B.getlength();
     B.insert(3,7);
         B.output();
         B.getlength();
-    B.dellist(5);
+    B.dellist(3);
         B.output();
         B.getlength();
     B.resetvalue(3,17);
